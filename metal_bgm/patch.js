8b941fc9ed200484002173b67e44c92600b07576
solagoll <evgenyfom2005@gmail.com>
2025-08-21 09:50:56 +0300

    Touhou 6 patch to replace bgm to metal arranges

---
+++ metal_bgm/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "metal_bgm",
+	"servers": [
+		"https://github.com/solagoll/touhou-patches"
+	],
+	"title": "Replace original bgm with metal arranges for TH06"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "id": "solagol",
+    "title": "solagol's bgm patches",
+    "contact": "evgenyfom2005@gmail.com",
+    "servers": [
+        "https://www.dropbox.com/scl/fo/cqgjyc7agfd6oy5sc2d2m/AHoX6jQpHiAiw3Cs0puu-Gg?rlkey=bp30xjax0zmimmm595mxfzqyw&st=oqe57zwq&dl=0"
+    ],
+    "patches": {
+        "metal_bgm": "Replace original bgm with metal arranges"
+    }
+}
